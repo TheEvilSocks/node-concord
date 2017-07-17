3fd004f9d32f30a6e3aa71ad2bf3566fe61e65c3
TheEvilSocks <theevilsocks@gmail.com>
2017-07-17 03:31:01 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ ! -d node_modules/ ]; then
 	echo "No node_modules directory found, setting up..."
+	echo "This may take a while... Please be patient."
 	npm install > concord_setup.log
 fi
 
